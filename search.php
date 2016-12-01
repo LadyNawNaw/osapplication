4a4e0be86bcde9a28b978ec12f3f65b3d86e2057
LadyNawNaw <nounouhass13@gmail.com>
2016-12-01 21:17:02 +0100

    Update search.php

--- search.php
+++ search.php
@@ -5,7 +5,7 @@ define('DB_PASS',getenv('OPENSHIFT_DB_PASSWORD'));
 define('DB_NAME',getenv('OPENSHIFT_DB_DBNAME'));
 // Create connection
 $conn = mysqli_connect(DB_HOST,DB_USER, DB_PASS,DB_NAME);
-$sql = "SELECT * FROM os where name like '".$_GET['name']."'";
+$sql = "SELECT * FROM os";// where name like '".$_GET['name']."'";
 $result = mysqli_query($conn, $sql);
 //if (mysqli_num_rows($result) > 0) {
 //$row = mysqli_fetch_assoc($result);
